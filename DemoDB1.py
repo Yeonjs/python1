c5101d763d8a12827cb51a3b45d454de6262d361
Yeonjs <yjsjsy0@gmail.com>
2021-04-08 10:16:28 +0900

    DB예예제

--- DemoDB1.py
+++ DemoDB1.py
@@ -16,9 +16,35 @@ name="gildong"
 phoneNumber="010-222"
 cur.execute("insert into PhoneBook values (?, ?);",(name,phoneNumber))
 #다중의 레코드(행, row)를 입력받는 경우 = 2차원 행렬데이터
-datalist=(("tom","010-123"),("weed","010-567"))
-cur.execute("insert into PhoneBook values (?, ?);", datalist))
+datalist=(("tom","010-123"),("lsd","010-567"))
+cur.executemany("insert into PhoneBook values (?, ?);", datalist)
 #검색
 cur.execute("select * from PhoneBook;")
-for row in cur:
-    print(row)
+# for row in cur:       #전체 출력
+#     print(row)
+
+#1건 검색
+print(cur.fetchone())
+#N건 검색
+print("---fetchMany(2)---")
+print(cur.fetchmany())
+print("===fetchall()===") 
+#위에 1건 검색하고 2건 검색 해서 다 처리된걸로 간주하고 나머지 하나만 출력됨
+#데이터가 사라진건 아닌데 버퍼가 날라간겨 
+cur.execute("select*from PhoneBook;")   #이코드 쓰면 위에꺼 좆까고 다출력됨
+print(cur.fetchall())
+print("------"*8)
+
+#결과를 슬라이싱
+cur.execute("select*from PhoneBook;") 
+result=cur.fetchone()
+print(result[0])
+print(result[1])
+
+print("==========다중행의 경우==========")
+#2차원 행렬 데이터 [행][렬]
+result=cur.fetchall()
+#이것도 마찬가지 위에 1번 사용되서 길동새끼부터 시작됨
+print(result[0][0])
+print(result[0][1])
+
